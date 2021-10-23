3cca252bfe2e60d88f5396fb3cb238ba0148fdf5
Asim <baralasim6@gmail.com>
2021-10-22 23:39:35 -0400

    added audio component with start and end times

---
+++ public/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Audio from "./components/Audio";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Audio url="https://freesound.org/data/previews/593/593675_9034501-lq.mp3" start="200" end="210"/>
+      
+
+      
+    </div>
+  );
+}
+
+export default App;
