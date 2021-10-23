e9d99c502d17c5c5d1746418c6d3249530c00888
Asim <baralasim6@gmail.com>
2021-10-23 15:29:49 -0400

    added more files

--- public/my-app/src/components/Slide.js
+++ public/my-app/src/components/Slide.js
@@ -3,11 +3,11 @@ import KeywordList from "./KeywordList";
 import "../css/Slide.css"
 
 const Slide = ({ url, keywords}) => {
-    
+
     return (
         <div className="slide-and-keywords">
-            <img className="current-slide" src={url} alt="A slide" width={"73.5%"} />
-            
+            <img className="current-slide" src={url} alt="A slide"  style={{ maxHeight: "80%" }} />
+            {/* bruh who thhis fool in front of us */}
             {/* <div class="vl" /> */}
             <KeywordList list={keywords}/>
         </div>
