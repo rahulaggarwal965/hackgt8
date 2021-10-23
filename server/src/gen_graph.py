da6f8eb1f52bda3b58b33616f86ee6c66683f7be
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2021-10-23 17:02:52 -0400

    add wav file, add graph generation

--- server/src/gen_graph.py
+++ server/src/gen_graph.py
@@ -1,5 +1,30 @@
 import math
 from textdistance import levenshtein
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+sw = stopwords.words('english')
+
+def phrase_similarity(a, b):
+    # don't want to worry about case sensitive
+    a, b = a.lower(), b.lower()
+
+    a_list, b_list = word_tokenize(a), word_tokenize(b)
+
+    a_set, b_set = {word for word in a_list if not word in sw}, {word for word in b_list if not word in sw}
+    l1, l2 = [], []
+
+    rvector = a_set.union(b_set) 
+    for word in rvector:
+        if word in a_set: l1.append(1)
+        else: l1.append(0)
+        if word in b_set: l2.append(1)
+        else: l2.append(0)
+    c = 0
+    for i in range(len(rvector)):
+            c+= l1[i]*l2[i]
+    cosine = c / float((sum(l1)*sum(l2))**0.5)
+    return cosine
 
 def generate_graph(slides):
     # first paragraph of each slide text will be automatically added to the global "priority hashmap"
@@ -12,22 +37,40 @@ def generate_graph(slides):
     memo = {}
 
     for slide in slides:
+        slide.references = []
         for keyword in slide.keywords: 
-            min_edit_distance = math.inf
+            max_similarity = -math.inf
             best_key = None
-            for key in references:
-                if (keyword, key) in memo:
-                    edit_distance = memo[(keyword, key)]
-                else:
-                    edit_distance = memo[(keyword, key)] = levenshtein(keyword, key)
-                if (edit_distance < min_edit_distance):
-                    min_edit_distance = edit_distance
-                    best_key = key
-            if (min_edit_distance > 5): # NOTE(rahul): 5 is arbitrary
-                # reference not found
-                references[keyword] = slide.index
+            similarities = [(phrase_similarity(keyword, key), references[key]) for key in references]
+            similarities = sorted(similarities, reverse=True)
+
+            if similarities[0][0] > 0.2:
+                slide.references.append([])
+                i = 0
+                while (similarities[i][0] > 0.2):
+                    slide.references[-1].append(similarities[i][1])
+                    i += 1
             else:
-                print(f"edit_distance[a={keyword}, b={best_key}] = {min_edit_distance}")
+                references[keyword] = slide.index
+                slide.references.append([slide.index])
+
+
+
+            # for key in references:
+            #     # if (keyword, key) in memo:
+            #     #     similarity = memo[(keyword, key)]
+            #     # else:
+            #     #     similarity = memo[(keyword, key)] = phrase_similarity(keyword, key)
+            #     similarity = phrase_similarity(keyword, key)
+            #     if (similarity > max_similarity):
+            #         max_similarity = similarity
+            #         best_key = key
+            # if (max_similarity < 0.2): # NOTE(rahul): 0.2 is arbitrary
+               # reference not found
+            #     references[keyword] = slide.index
+            # else:
+            #     print(f"dist[a={keyword}, b={best_key}] = {max_similarity}")
+            #     print(f"a.index = {slide.index}, b.index = {references[best_key]}")
 
 
                 
--- server/src/main.py
+++ server/src/main.py
@@ -1,19 +1,20 @@
 import os
 import json
 
-VIDEO_FILE = "../../data/4 Cellular respiration slides 19-30-tUVjJ8ak3LU2.mp4"
 from constants import DATA_DIR
 from process import process_video
 from gen_graph import generate_graph
-from slide import Slide
+from slide import Slide, serialize_slides
 
 # TODO(rahul): make these filepaths work on all systems no matter where the main file is run from
 VIDEO_FILE = "4 Cellular respiration slides 19-30-tUVjJ8ak3LU.mp4"
 
-PROCESS=False
+PROCESS=True
 
 if PROCESS:
-    process_video(VIDEO_FILE)
+    slides = process_video(VIDEO_FILE)
+    generate_graph(slides)
+    serialize_slides(VIDEO_FILE, slides)
 else:
     SERIALIZED_SLIDES = f"{DATA_DIR}/4 Cellular respiration slides 19-30-tUVjJ8ak3LU/serialized_slides.json"
 
@@ -24,9 +25,6 @@ else:
     
         for d in data:
             slides.append(Slide.deserialize(d))
-            print(slides[-1])
 
-        
-    graph = generate_graph(slides)
-    print(graph)
+    generate_graph(slides)
 
