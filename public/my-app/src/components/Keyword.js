f0a58ef431043a2cf717fdf8c044287130a69bfc
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2021-10-24 04:17:50 -0400

    refactor to material ui

--- public/my-app/src/components/Keyword.js
+++ public/my-app/src/components/Keyword.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import KeywordList from "./KeywordList";
 import "../css/Slide.css"
 import Modal from "react-modal";
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
 Modal.setAppElement("#root");
+
 const Keyword = ({ word, list}) => {
     const [isModalOn, setIsModalOn] = useState(false);
     return (
-        <>
-            <span className="keyword" onClick={e => console.log(list)}>{word}</span>
-        </>
-
+        <ListItem disablePadding>
+            <ListItemButton onClick={e => console.log(list)}>
+                <ListItemText sx={{ "color": "#b3b3b3" }}primary={word}/>                
+            </ListItemButton>
+        </ListItem>
     );
 
 };
